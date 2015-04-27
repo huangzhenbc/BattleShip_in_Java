a44fa81d22e50c7c8551400d8e9049519a782163
zhaozhao0906 <jiny1106@outlook.com>
2015-04-27 20:16:44 +0800

    Revert "测试3"
    This reverts commit 510331632531ecf3a3b04b4674abd3bb4b33e0f0.

--- game/src/game/TestGame.java
+++ game/src/game/TestGame.java
@@ -15,6 +15,7 @@ public class TestGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
 	// Test your class here
     }
     
